0df024f303b7e7db92230c9fa54492bf1994e0fb
hanthidoan12-tech <hanthidoan12@gmail.com>
2026-02-13 21:45:17 +0800

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,14 @@
 self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open("betwin-cache").then(function(cache) {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/script.js",
-        "/thankyou.html"
-      ]);
-    })
-  );
+event.waitUntil(
+caches.open("betwin-cache").then(function(cache) {
+return cache.addAll([
+"/",
+"/index.html",
+"/thankyou.html",
+"/style.css",
+"/script.js",
+"/manifest.json"
+]);
+})
+);
 });
