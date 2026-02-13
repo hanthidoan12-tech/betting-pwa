c0519d23587ea0a3deb711ed1e911d6cb5b36bc4
hanthidoan12-tech <hanthidoan12@gmail.com>
2026-02-13 21:44:44 +0800

    script.js

--- script.js
+++ script.js
@@ -4,9 +4,36 @@ document.getElementById("form").scrollIntoView({ behavior: "smooth" });
 
 document.getElementById("regForm").addEventListener("submit", function(e){
 e.preventDefault();
+
+const loader = document.getElementById("loader");
+loader.classList.remove("hidden");
+
+setTimeout(() => {
 window.location.href = "thankyou.html";
+}, 2000);
 });
+
 
 if ("serviceWorker" in navigator) {
 navigator.serviceWorker.register("service-worker.js");
 }
+
+
+// Install button logic
+let deferredPrompt;
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+e.preventDefault();
+deferredPrompt = e;
+installBtn.style.display = "inline-block";
+});
+
+installBtn.addEventListener("click", () => {
+if (deferredPrompt) {
+deferredPrompt.prompt();
+deferredPrompt.userChoice.then(() => {
+deferredPrompt = null;
+});
+}
+});
